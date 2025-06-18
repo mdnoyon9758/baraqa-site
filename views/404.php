6e09c79e761c8e447a95c1d04a2754b32b499e17
Md Noyon <mdnoyon9758@gmail.com>
2025-06-18 18:52:43 +0600

    Feat: Implement Front Controller pattern for robust routing

---
+++ views/404.php
@@ -0,0 +1,10 @@
+<?php 
+$page_title = "Page Not Found";
+require_once __DIR__ . '/../includes/header.php'; 
+?>
+<div class="container text-center py-5">
+    <h1 class="display-1">404</h1>
+    <p class="lead">Oops! The page you are looking for does not exist.</p>
+    <a href="/" class="btn btn-primary">Go to Homepage</a>
+</div>
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
